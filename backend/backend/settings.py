985354ca3559d9bc527e210361daee8ce0f81b23
rafayc179 <rxc570@case.edu>
2024-03-28 16:16:30 -0400

    media urls should be working

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,6 +116,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'mediafiles'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from CSDS393PROJECT import views
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -32,3 +34,4 @@ urlpatterns = [
 ]
 
 urlpatterns= format_suffix_patterns(urlpatterns)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
